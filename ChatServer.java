97d68e3e4aca8d142ddbe7b3b2a7b2b8d64bd124
Yuxiang Hong <89092053+YSJHYX@users.noreply.github.com>
2024-04-22 14:51:51 -0700

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -2,7 +2,50 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    
+
+    String messages = "";
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return messages;
+        }
+        else if (url.getPath().equals("/add-message")) {
+            String query = url.getQuery();
+            String[] params = query.split("&");
+            String user = "";
+            String message = "";
+
+            for (String para: params) {
+                String[] input = param.split("=");
+                if (input[0].equals("s")) {
+                    message = input[1];
+                }
+                else if (input[0].equals("user")) {
+                    user = input[1];
+                }
+            }
+
+            if (!user.isEmpty() && !message.isEmpty()) {
+                messages += user + ": " + message + "\n";
+            }
+            return messages;
+        }
+        else {
+            return "Error!";
+        }
+    }
+}
+
+
+class ChatServer {
+    public static void main(String[] args) throws IOException{
+        if(args.length == 0) {
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+        }
+        
+        int portNum = Integer.parseInt(args[0]);
+
+        Server.start(portNum, new ChatServer());
+    }
 }
 
 
